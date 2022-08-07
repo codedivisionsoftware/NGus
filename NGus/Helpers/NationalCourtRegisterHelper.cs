873fc09e920e76fdbc51174b350c876c6b8339a9
Radosław Bujak <kontakt@codedivision.pl>
2022-08-08 00:32:40 +0200

    Commit

--- NGus/Helpers/NationalCourtRegisterHelper.cs
+++ NGus/Helpers/NationalCourtRegisterHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 using NGus.Models;
@@ -25,21 +26,86 @@ namespace NGus.Helpers
                 return null;
             }
 
-            var nationalCourtRegisters = new List<NationalCourtRegisterModel>();
-
             var xRoot = xDocument.Root;
             var xData = xRoot?.Elements("dane").ToList();
+
+            var nationalCourtRegisters = new List<NationalCourtRegisterModel>();
 
             if (xData != null && xData.Any())
             {
                 foreach (var xSingleData in xData)
                 {
+                    if (xSingleData == null)
+                    {
+                        continue;
+                    }
+
+                    var xNationalBusinessRegistryNumber = xSingleData?.Element("Regon");
+                    var nationalBusinessRegistryNumber = !string.IsNullOrEmpty(xNationalBusinessRegistryNumber?.Value) && !string.IsNullOrWhiteSpace(xNationalBusinessRegistryNumber.Value) ? xNationalBusinessRegistryNumber.Value : null;
+
+                    var xTaxpayerIdentificationNumber = xSingleData?.Element("Nip");
+                    var taxpayerIdentificationNumber = !string.IsNullOrEmpty(xTaxpayerIdentificationNumber?.Value) && !string.IsNullOrWhiteSpace(xTaxpayerIdentificationNumber.Value) ? xTaxpayerIdentificationNumber.Value : null;
+
+                    var xTaxpayerIdentificationNumberStatus = xSingleData?.Element("StatusNip");
+                    var taxpayerIdentificationNumberStatus = !string.IsNullOrEmpty(xTaxpayerIdentificationNumberStatus?.Value) && !string.IsNullOrWhiteSpace(xTaxpayerIdentificationNumberStatus.Value) ? xTaxpayerIdentificationNumberStatus.Value : null;
+
                     var xName = xSingleData?.Element("Nazwa");
                     var name = !string.IsNullOrEmpty(xName?.Value) && !string.IsNullOrWhiteSpace(xName.Value) ? xName.Value : null;
 
+                    var xVoivodeship = xSingleData?.Element("Wojewodztwo");
+                    var voivodeship = !string.IsNullOrEmpty(xVoivodeship?.Value) && !string.IsNullOrWhiteSpace(xVoivodeship.Value) ? xVoivodeship.Value : null;
+
+                    var xDistrict = xSingleData?.Element("Powiat");
+                    var district = !string.IsNullOrEmpty(xDistrict?.Value) && !string.IsNullOrWhiteSpace(xDistrict.Value) ? xDistrict.Value : null;
+
+                    var xCommune = xSingleData?.Element("Gmina");
+                    var commune = !string.IsNullOrEmpty(xCommune?.Value) && !string.IsNullOrWhiteSpace(xCommune.Value) ? xCommune.Value : null;
+
+                    var xLocality = xSingleData?.Element("Miejscowosc");
+                    var locality = !string.IsNullOrEmpty(xLocality?.Value) && !string.IsNullOrWhiteSpace(xLocality.Value) ? xLocality.Value : null;
+
+                    var xZipCode = xSingleData?.Element("KodPocztowy");
+                    var zipCode = !string.IsNullOrEmpty(xZipCode?.Value) && !string.IsNullOrWhiteSpace(xZipCode.Value) ? xZipCode.Value : null;
+
+                    var xStreet = xSingleData?.Element("Ulica");
+                    var street = !string.IsNullOrEmpty(xStreet?.Value) && !string.IsNullOrWhiteSpace(xStreet.Value) ? xStreet.Value : null;
+
+                    var xPropertyNumber = xSingleData?.Element("NrNieruchomosci");
+                    var propertyNumber = !string.IsNullOrEmpty(xPropertyNumber?.Value) && !string.IsNullOrWhiteSpace(xPropertyNumber.Value) ? xPropertyNumber.Value : null;
+
+                    var xApartmentNumber = xSingleData?.Element("NrLokalu");
+                    var apartmentNumber = !string.IsNullOrEmpty(xApartmentNumber?.Value) && !string.IsNullOrWhiteSpace(xApartmentNumber.Value) ? xApartmentNumber.Value : null;
+
+                    var xType = xSingleData?.Element("Typ");
+                    var type = !string.IsNullOrEmpty(xType?.Value) && !string.IsNullOrWhiteSpace(xType.Value) ? xType.Value : null;
+
+                    var xSilosId = xSingleData?.Element("SilosID");
+                    var silosId = !string.IsNullOrEmpty(xSilosId?.Value) && !string.IsNullOrWhiteSpace(xSilosId.Value) && int.TryParse(xSilosId.Value, out _) ? (int?) Convert.ToInt32(xSilosId.Value) : null;
+
+                    var xTerminationActivityDate = xSingleData?.Element("DataZakonczeniaDzialalnosci");
+                    var terminationActivityDate = !string.IsNullOrEmpty(xTerminationActivityDate?.Value) && !string.IsNullOrWhiteSpace(xTerminationActivityDate.Value) && DateTime.TryParse(xTerminationActivityDate.Value, out _) ? (DateTime?) Convert.ToDateTime(xTerminationActivityDate.Value) : null;
+
+                    var xPostOfficeLocation = xSingleData?.Element("MiejscowoscPoczty");
+                    var postOfficeLocation = !string.IsNullOrEmpty(xPostOfficeLocation?.Value) && !string.IsNullOrWhiteSpace(xPostOfficeLocation.Value) ? xPostOfficeLocation.Value : null;
+
                     nationalCourtRegisters.Add(new NationalCourtRegisterModel
                     {
-                        Name = name
+                        NationalBusinessRegistryNumber = nationalBusinessRegistryNumber,
+                        TaxpayerIdentificationNumber = taxpayerIdentificationNumber,
+                        TaxpayerIdentificationNumberStatus = taxpayerIdentificationNumberStatus,
+                        Name = name,
+                        Voivodeship = voivodeship,
+                        District = district,
+                        Commune = commune,
+                        Locality = locality,
+                        ZipCode = zipCode,
+                        Street = street,
+                        PropertyNumber = propertyNumber,
+                        ApartmentNumber = apartmentNumber,
+                        Type = type,
+                        SilosId = silosId,
+                        TerminationActivityDate = terminationActivityDate,
+                        PostOfficeLocation = postOfficeLocation
                     });
                 }
             }
--- NGus/Models/NationalCourtRegisterModel.cs
+++ NGus/Models/NationalCourtRegisterModel.cs
@@ -1,4 +1,6 @@
-﻿namespace NGus.Models
+﻿using System;
+
+namespace NGus.Models
 {
     public class NationalCourtRegisterModel
     {
@@ -6,5 +8,17 @@
         public string TaxpayerIdentificationNumber { get; set; }
         public string TaxpayerIdentificationNumberStatus { get; set; }
         public string Name { get; set; }
+        public string Voivodeship { get; set; }
+        public string District { get; set; }
+        public string Commune { get; set; }
+        public string Locality { get; set; }
+        public string ZipCode { get; set; }
+        public string Street { get; set; }
+        public string PropertyNumber { get; set; }
+        public string ApartmentNumber { get; set; }
+        public string Type { get; set; }
+        public int? SilosId { get; set; }
+        public DateTime? TerminationActivityDate { get; set; }
+        public string PostOfficeLocation { get; set; }
     }
 }
